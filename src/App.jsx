c549959ca95a435b9cef1d8842ea97b9b7ce29bb
unknown <you@example.com>
2024-07-08 22:17:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import MainContent from './components/MainContent';
+import Container from '@mui/material/Container';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div style={{display: "flex", justifyContent:"center", width: "100vw"}}>
+        <Container maxWidth="xl">
+          <MainContent/>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default App
